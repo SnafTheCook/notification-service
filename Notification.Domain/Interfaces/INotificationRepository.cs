21e4a6c896c1c0539a5db8649d94ca2b0c9c674c
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-03-20 19:33:50 +0100

    refactor: define INotificationRepository interface

---
+++ Notification.Domain/Interfaces/INotificationRepository.cs
@@ -0,0 +1,17 @@
+﻿using Notification.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Domain.Interfaces
+{
+    public interface INotificationRepository
+    {
+        Task TaskAsync(NotificationEntity notification);
+        Task UpdateAsync(NotificationEntity notification);
+        Task<NotificationEntity> GetByIdAsync(Guid id);
+        Task<IEnumerable<NotificationEntity>> GetPendingRetryAsync();
+    }
+}
