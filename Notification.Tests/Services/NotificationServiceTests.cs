2b947acb31441c07a6959b6f805d951e962b8ad4
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-04-04 10:35:42 +0200

    test: setup orchestrator mocks

---
+++ Notification.Tests/Services/NotificationServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Notification.Domain.Interfaces;
+using Notification.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Tests.Services
+{
+    public class NotificationServiceTests
+    {
+        private readonly Mock<INotificationRepository> _mockRepo = new();
+        private readonly Mock<NotificationDispatcher> _mockDispatcher;
+        private readonly Mock<ILogger<NotificationService>> _mockLogger = new();
+        private readonly NotificationService _notificationService;
+
+        public NotificationServiceTests()
+        {
+            _mockDispatcher = new Mock<NotificationDispatcher>(new List<INotificationProvider>(), null!, null!);
+            _notificationService = new NotificationService(_mockRepo.Object, _mockDispatcher.Object, _mockLogger.Object);
+        }
+    }
+}
