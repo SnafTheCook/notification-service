f63998c758088249dd1b78d22df1dea1cb53a2dc
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-04-05 11:42:03 +0200

    feat: implement global error handling

---
+++ Notification.Api/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Notification.Domain.Wrappers;
+
+namespace Notification.Api.Middleware
+{
+    public class ErrorHandlerMiddleware(RequestDelegate next, ILogger<ErrorHandlerMiddleware> logger)
+    {
+        public async Task Invoke(HttpContext context)
+        {
+            try 
+            { 
+                await next(context); 
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsJsonAsync(ApiResponse.Fail("Internal Server Error"));
+            }
+        }
+    }
+}
