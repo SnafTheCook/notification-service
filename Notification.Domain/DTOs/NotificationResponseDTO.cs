11327bb0145b45e3f05a8ec9c1d26a45a6c1f827
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-04-11 14:02:03 +0200

    feat: add notification response dto

---
+++ Notification.Domain/DTOs/NotificationResponseDTO.cs
@@ -0,0 +1,20 @@
+﻿using Notification.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Domain.DTOs
+{
+    public record NotificationResponseDTO(
+        Guid id,
+        string Recipient,
+        string Content,
+        ChannelType Channel,
+        NotificationStatus Status,
+        string? SucceededProvider,
+        DateTime CreatedAt,
+        DateTime? LastModifiedAt
+        );
+}
