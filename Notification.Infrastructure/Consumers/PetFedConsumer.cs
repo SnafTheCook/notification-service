37db21a62299e02775b2f09adb119d3d88caf428
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-04-13 09:58:38 +0200

    feat: implement PetFedEvent consumer

---
+++ Notification.Infrastructure/Consumers/PetFedConsumer.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using Notification.Domain.Enums;
+using Notification.Domain.Events;
+using Notification.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Infrastructure.Consumers
+{
+    public class PetFedConsumer(INotificationService notificationService) : IConsumer<PetFedEvent>
+    {
+        public async Task Consume(ConsumeContext<PetFedEvent> context)
+        {
+            var @event = context.Message;
+
+            await notificationService.ProcessNotificationAsync(
+                @event.OwnerEmail,
+                $"Your pet {@event.PetName} was just fed!",
+                ChannelType.Email);
+        }
+    }
+}
